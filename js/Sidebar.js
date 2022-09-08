2a407315941a8ca4cefac8c89aa68e9cdef0a1c9
Lucas Verhulst <lucasverhulst@hotmail.nl>
2022-09-08 08:56:32 +0200

    added hambuger menu

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("sidenav").style.width = "250px";
+}
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
